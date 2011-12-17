8fa7df3cef7377193190d4d170a1ede9bb663b9c
JeanDo <JeanDo@localhost>
2011-12-17 17:22:59 +0100

    Minor FRENCH cleanups

--- code_part1/OSTC_code_asm_part1/definitions.asm
+++ code_part1/OSTC_code_asm_part1/definitions.asm
@@ -31,7 +31,7 @@
 ; International extension. Selecting messages source:
 ;#DEFINE    ENGLISH                         ; Use english_text.asm
 ;#DEFINE	FRENCH  						; Use french_text.asm
-#DEFINE	GERMAN							; Use german_text.asm
+;#DEFINE	GERMAN							; Use german_text.asm
 ;#DEFINE	SPANISH							; Use spanish_text.asm
 ;#DEFINE	RUSSIAN							; Use russian_text.asm
 ;#DEFINE	TURKISH							; Use turkish_text.asm

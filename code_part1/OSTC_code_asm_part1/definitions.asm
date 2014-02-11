53a269c8d843ac129587e2a2c659ba4de374cd2b
heinrichsweikamp <heinrichsweikamp@localhost>
2014-02-11 12:40:16 +0100

    2.91beta start

--- code_part1/OSTC_code_asm_part1/definitions.asm
+++ code_part1/OSTC_code_asm_part1/definitions.asm
@@ -22,9 +22,9 @@
 ; ToDo:
 
 #DEFINE	softwareversion_x		d'2'		; Software version  XX.YY
-#DEFINE	softwareversion_y		d'90'		; Software version  XX.YY
+#DEFINE	softwareversion_y		d'91'		; Software version  XX.YY
 
-#DEFINE softwareversion_beta 	0 			; (and 0 for release)
+#DEFINE softwareversion_beta 	1 			; (and 0 for release)
 
 #DEFINE	max_custom_number		d'73'		; Number of last used custom function
 
